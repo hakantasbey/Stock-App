2095f89c4bd8f1989b4b11d70ab1279eada22567
hakan <tasbeyhakan@gmail.com>
2024-01-10 14:06:11 +0300

    useAxios added

--- src/service/useAuthCalls.jsx
+++ src/service/useAuthCalls.jsx
@@ -8,20 +8,24 @@ import {
   logoutSuccess,
   registerSuccess,
 } from "../features/authSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+// import { useSelector } from "react-redux";
+import useAxios from "./useAxios";
 
 const useAuthCalls = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const {token} = useSelector(state => state.auth)
+  // const {token} = useSelector(state => state.auth)
+  const { axiosWithToken, axiosPublic } = useAxios()
 
   const login = async (userInfo) => {
     dispatch(fetchStart());
     try {
-      const { data } = await axios.post(
-        `${process.env.REACT_APP_BASE_URL}/auth/login/`,
-        userInfo
-      );
+      // const { data } = await axios.post(
+      //   `${process.env.REACT_APP_BASE_URL}/auth/login/`,
+      //   userInfo
+      // );
+      const { data } = await axiosPublic.post("/auth/login/", userInfo)
       dispatch(loginSuccess(data));
       toastSuccessNotify("Login işlemi basarili.");
       navigate("/stock");
@@ -49,9 +53,10 @@ const useAuthCalls = () => {
   const logout = async () => {
     dispatch(fetchStart());
     try {
-      await axios.get(`${process.env.REACT_APP_BASE_URL}/auth/logout`,{
-          headers: { Authorization: `Token ${token}` },
-        });
+      // await axios.get(`${process.env.REACT_APP_BASE_URL}/auth/logout`,{
+      //     headers: { Authorization: `Token ${token}` },
+      //   });
+      await axiosWithToken.get("auth/logout")
       toastSuccessNotify("Logout işlemi basarili.");
       dispatch(logoutSuccess());
       // navigate("/");
--- src/service/useAxios.jsx
+++ src/service/useAxios.jsx
@@ -1,15 +1,18 @@
-import axios from "axios";
-import { useSelector } from "react-redux";
-
-
+import axios from "axios"
+import { useSelector } from "react-redux"
 
 const useAxios = () => {
-  const {token} = useSelector(state => state.auth)
+  const { token } = useSelector((state) => state.auth)
+
   const axiosWithToken = axios.create({
     baseURL: `${process.env.REACT_APP_BASE_URL}`,
-    headers: {'Authorization': `Token ${token}`}
-  });
-  return {axiosWithToken}
+    headers: { Authorization: `Token ${token}` },
+  })
+
+  const axiosPublic = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+  })
+  return { axiosWithToken, axiosPublic }
 }
 
 export default useAxios
