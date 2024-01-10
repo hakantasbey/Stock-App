997b905507a5098dd4d2f23fe2447adc1c9f5073
hakan <tasbeyhakan@gmail.com>
2024-01-10 12:58:08 +0300

    axios instance added

---
+++ src/service/useAxios.jsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+
+
+const useAxios = () => {
+  const {token} = useSelector(state => state.auth)
+  const axiosWithToken = axios.create({
+    baseURL: `${process.env.REACT_APP_BASE_URL}`,
+    headers: {'Authorization': `Token ${token}`}
+  });
+  return {axiosWithToken}
+}
+
+export default useAxios
+
+
