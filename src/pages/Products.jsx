095f1c8467156039bd5db5f780ee772aa4c70dd5
hakan <tasbeyhakan@gmail.com>
2024-01-13 12:14:49 +0300

    feat: Product table starter

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,53 @@
-
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+import { useEffect, useState } from "react"
+import useStockCalls from "../service/useStockCalls"
+import { useSelector } from "react-redux"
+import { Grid } from "@mui/material"
+import ProductModal from "../components/ProductModal"
+import ProductTable from "../components/ProductTable"
 
 const Products = () => {
+  // const { getFirms, getSales } = useStockCalls()
+  const { getStocks } = useStockCalls()
+  const { products } = useSelector((state) => state.stock)
+
+  const [info, setInfo] = useState({
+    name: "",
+    phone: "",
+    address: "",
+    image: "",
+  })
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => {
+    setOpen(false)
+    setInfo({ name: "", phone: "", address: "", image: "" })
+  }
+
+  useEffect(() => {
+    getStocks("products")
+  }, [])
+
   return (
-    <div>Products</div>
+    <div>
+      <Typography variant="h4" color="error" mb={3}>
+        Products
+      </Typography>
+      <Button variant="contained" sx={{mb:"3"}} onClick={handleOpen}>
+        New Product
+      </Button>
+
+      <ProductModal
+        open={open}
+        handleClose={handleClose}
+        info={info}
+        setInfo={setInfo}
+      />
+
+      <ProductTable />
+    </div>
   )
 }
 
